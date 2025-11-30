230ed023f1304e4819942d35c85a881d4e45d340
osvaldosereia <Junior.sereia@hotmail.com>
2025-11-30 17:04:49 -0300

    Update and rename cadastro.json to cadastro.js

---
+++ data/cadastro.js
@@ -0,0 +1,398 @@
+const clientes = [
+   {
+    "nome": "MARTA Chávez infante",
+    "telefone": "65981199305",
+    "cidade": "Cuiabá",
+    "bairro": "Planaltos"
+  },
+  {
+    "nome": "Sandra Amelia Concepción Castillo",
+    "telefone": "95984184702",
+    "cidade": "Cuiabá",
+    "bairro": "Bela vista"
+  },
+  {
+    "nome": "Cleverton dos Santos aguiar",
+    "telefone": "65981014978",
+    "cidade": "Cuiabá",
+    "bairro": "Pesadinho liberdade"
+  },
+  {
+    "nome": "Marileivis borrero Sotomayor",
+    "telefone": "65999105269",
+    "cidade": "Várzea Grande",
+    "bairro": "Cristo rei"
+  },
+  {
+    "nome": "Yesenia Mendez",
+    "telefone": "65992132971",
+    "cidade": "Cuiabá",
+    "bairro": "Carubem"
+  },
+  {
+    "nome": "ABNER HENRIQUE DE BRITO MENDES",
+    "telefone": "65996157273",
+    "cidade": "Cuiabá",
+    "bairro": "Jardim fortaleza"
+  },
+  {
+    "nome": "Nilza Maria da Luz grizostimo de Oliveira",
+    "telefone": "65992940860",
+    "cidade": "Cuiabá",
+    "bairro": "Residencial nico baracat 2"
+  },
+  {
+    "nome": "Misleidys Arango",
+    "telefone": "65981125329",
+    "cidade": "Cuiabá",
+    "bairro": "Bela vista"
+  },
+  {
+    "nome": "Vanuza Delfina de Campos Conceição",
+    "telefone": "65984588706",
+    "cidade": "Várzea Grande",
+    "bairro": "Ikarai"
+  },
+  {
+    "nome": "Lauriane cristina",
+    "telefone": "65996219391",
+    "cidade": "Cuiabá",
+    "bairro": "Residência sucuri"
+  },
+  {
+    "nome": "Lucas França silva",
+    "telefone": "65996648093",
+    "cidade": "Cuiabá",
+    "bairro": "Residencial buritis"
+  },
+  {
+    "nome": "Eumara do Carmo Paz",
+    "telefone": "65999296811",
+    "cidade": "Cuiabá",
+    "bairro": "Residencial Nova Canaã II"
+  },
+  {
+    "nome": "ARCILEI MARIA LUCIALDO PEIXOTO",
+    "telefone": "65992296707",
+    "cidade": "Cuiabá",
+    "bairro": "CPA 1"
+  },
+  {
+    "nome": "Cleonice da Silva",
+    "telefone": "65992132865",
+    "cidade": "Cuiabá",
+    "bairro": "Santa Terezinha II"
+  },
+  {
+    "nome": "MATHEUS GUILHERME SOUZA",
+    "telefone": "65984552108",
+    "cidade": "Cuiabá",
+    "bairro": "Jardim Renacer"
+  },
+  {
+    "nome": "Yasmin Brito da Costa",
+    "telefone": "65981374762",
+    "cidade": "Cuiabá",
+    "bairro": "Parque Jardim universitário"
+  },
+  {
+    "nome": "Mauro Arruda",
+    "telefone": "65999452618",
+    "cidade": "Cuiabá",
+    "bairro": "São Francisco"
+  },
+  {
+    "nome": "Maria de Jesus Barbosa de Freitas",
+    "telefone": "65981346555",
+    "cidade": "Cuiabá",
+    "bairro": "Alvorada"
+  },
+  {
+    "nome": "Rut Marta Seritelli Silva",
+    "telefone": "65996316052",
+    "cidade": "Cuiabá",
+    "bairro": "Cidade verde"
+  },
+  {
+    "nome": "Eliete Santos paes de Oliveira",
+    "telefone": "65992081592",
+    "cidade": "Cuiabá",
+    "bairro": "Coxipo"
+  },
+  {
+    "nome": "Juliana Pereira Moreira",
+    "telefone": "65993271916",
+    "cidade": "Várzea Grande",
+    "bairro": "Paiaguais"
+  },
+  {
+    "nome": "YADIRA BOFFILL CASTILLO",
+    "telefone": "95984073899",
+    "cidade": "Cuiabá",
+    "bairro": "Residencial São Carlos"
+  },
+  {
+    "nome": "Luana jesus de arruda",
+    "telefone": "65996129849",
+    "cidade": "Cuiabá",
+    "bairro": "Contorno Leste"
+  },
+  {
+    "nome": "Olivelton Augusto lemos Silva",
+    "telefone": "65992437621",
+    "cidade": "Cuiabá",
+    "bairro": "Morada da Serra"
+  },
+  {
+    "nome": "Josimar jose de Jesus",
+    "telefone": "65981001546",
+    "cidade": "Cuiabá",
+    "bairro": "1de marco"
+  },
+  {
+    "nome": "Luvis Neyda Chavez Infante",
+    "telefone": "981439228",
+    "cidade": "Cuiabá",
+    "bairro": "Planalto"
+  },
+  {
+    "nome": "Geyce De Andrade Oliveira",
+    "telefone": "65981014979",
+    "cidade": "Cuiabá",
+    "bairro": "Liberdade"
+  },
+  {
+    "nome": "María das Graças da silva",
+    "telefone": "65993346389",
+    "cidade": "Várzea Grande",
+    "bairro": "Marajuara"
+  },
+  {
+    "nome": "Rogério Conceição de faria",
+    "telefone": "65992257688",
+    "cidade": "Cuiabá",
+    "bairro": "Jd Florianópolis"
+  },
+  {
+    "nome": "Ana Carla Neves Lima",
+    "telefone": "65992199419",
+    "cidade": "Várzea Grande",
+    "bairro": "Residencial Novo Mundo"
+  },
+  {
+    "nome": "Luis Arquimedes rosário suniaga",
+    "telefone": "65981412440",
+    "cidade": "Cuiabá",
+    "bairro": "Morada da cerra"
+  },
+  {
+    "nome": "Luzinete da Silva Gomes",
+    "telefone": "65984063545",
+    "cidade": "Várzea Grande",
+    "bairro": "Da manga cristo rei"
+  },
+  {
+    "nome": "Fabián Dariel Pérez ramirez",
+    "telefone": "95984210936",
+    "cidade": "Cuiabá",
+    "bairro": "8 de abril"
+  },
+  {
+    "nome": "Ivete vitoriano vitorino",
+    "telefone": "92345531",
+    "cidade": "Várzea Grande",
+    "bairro": "Marajuara"
+  },
+  {
+    "nome": "Adriane Cardoso",
+    "telefone": "65993245786",
+    "cidade": "Cuiabá",
+    "bairro": "Tijucal"
+  },
+  {
+    "nome": "Roselis cortina Martinez",
+    "telefone": "65992764694",
+    "cidade": "Cuiabá",
+    "bairro": "Bela vista"
+  },
+  {
+    "nome": "Lucimeire Vitorino da Silva Drewlo",
+    "telefone": "66996061878",
+    "cidade": "Várzea Grande",
+    "bairro": "Marajoara"
+  },
+  {
+    "nome": "Celina Maria Morais Costa",
+    "telefone": "992567529",
+    "cidade": "Cuiabá",
+    "bairro": "Tijucal"
+  },
+  {
+    "nome": "Amarilys Ramirez castillo",
+    "telefone": "9584032049",
+    "cidade": "Cuiabá",
+    "bairro": "8 de abril"
+  },
+  {
+    "nome": "Aneliet Gamboa Mejias",
+    "telefone": "65993414565",
+    "cidade": "Cuiabá",
+    "bairro": "Bela vista"
+  },
+  {
+    "nome": "EMINIELLY PEREIRA DE SOUZA",
+    "telefone": "65992811718",
+    "cidade": "Cuiabá",
+    "bairro": "Jd Santa Izabel"
+  },
+  {
+    "nome": "Kamila Barbosa da silva",
+    "telefone": "65996640238",
+    "cidade": "Cuiabá",
+    "bairro": "Novo paraíso"
+  },
+  {
+    "nome": "Hilda Yannoris Salazar Cedeno",
+    "telefone": "65992684416",
+    "cidade": "Várzea Grande",
+    "bairro": "Cristo Rey"
+  },
+  {
+    "nome": "IASMIM KAROLINE BATISTA SANTOS",
+    "telefone": "65993518831",
+    "cidade": "Várzea Grande",
+    "bairro": "São Simão"
+  },
+  {
+    "nome": "Noelvis Castillo Chavez",
+    "telefone": "9584257073",
+    "cidade": "Cuiabá",
+    "bairro": "Vela Vista"
+  },
+  {
+    "nome": "Adrieli Verônica Dourado Drewlo",
+    "telefone": "66999190985",
+    "cidade": "Várzea Grande",
+    "bairro": "Marajoara 1"
+  },
+  {
+    "nome": "Reginaldo José Oliveira Santos Silva",
+    "telefone": "65992119942",
+    "cidade": "Várzea Grande",
+    "bairro": "Vila São João"
+  },
+  {
+    "nome": "Letícia Ferreira",
+    "telefone": "65984679143",
+    "cidade": "Cuiabá",
+    "bairro": "Jd Florianópolis"
+  },
+  {
+    "nome": "KAYLLAYNE JAMILY ALMEIDA SILVA",
+    "telefone": "65992406181",
+    "cidade": "Várzea Grande",
+    "bairro": "Serra dourada"
+  },
+  {
+    "nome": "José Maria de FIGUEIREDO Junior",
+    "telefone": "65984782146",
+    "cidade": "Várzea Grande",
+    "bairro": "Bairro da manga"
+  },
+  {
+    "nome": "Daniela pinto dos Santos",
+    "telefone": "65996311327",
+    "cidade": "Várzea Grande",
+    "bairro": "São Mateus"
+  },
+  {
+    "nome": "Larissa Gabrielle Oliveira e silva",
+    "telefone": "65992707731",
+    "cidade": "Cuiabá",
+    "bairro": "Alice novack"
+  },
+  {
+    "nome": "JOZIANY LAURA NASSARDEN SILVA",
+    "telefone": "65999944293",
+    "cidade": "Várzea Grande",
+    "bairro": "Marajoara"
+  },
+  {
+    "nome": "Ninoska Andrade",
+    "telefone": "95981175560",
+    "cidade": "Cuiabá",
+    "bairro": "Morada da serra"
+  },
+  {
+    "nome": "Kelly Aparecida",
+    "telefone": "65992037326",
+    "cidade": "Cuiabá",
+    "bairro": "Tijucal"
+  },
+  {
+    "nome": "Elizabete Dias dos santos malaguetta",
+    "telefone": "65981490416",
+    "cidade": "Várzea Grande",
+    "bairro": "Pirineo"
+  },
+  {
+    "nome": "Simone tatiana pimenta",
+    "telefone": "65999820431",
+    "cidade": "Cuiabá",
+    "bairro": "Residencial são carlos"
+  },
+  {
+    "nome": "Stefany silva",
+    "telefone": "65992171980",
+    "cidade": "Várzea Grande",
+    "bairro": "Parque do lago"
+  },
+  {
+    "nome": "VANIA REGINA BATISTA AMORIM FERREIRA",
+    "telefone": "65984773145",
+    "cidade": "Várzea Grande",
+    "bairro": "Jardim primavera"
+  },
+  {
+    "nome": "Márcia Figueiredo dos Santos",
+    "telefone": "65992938380",
+    "cidade": "Cuiabá",
+    "bairro": "Industriário 1"
+  },
+  {
+    "nome": "Jakelliny de Oliveira leite",
+    "telefone": "65993036232",
+    "cidade": "Cuiabá",
+    "bairro": "Primeiro de março"
+  },
+  {
+    "nome": "Marcos Kleyton Nishida",
+    "telefone": "65993368890",
+    "cidade": "Cuiabá",
+    "bairro": "Paiaguás"
+  },
+  {
+    "nome": "Jucilene bispo dos Santos",
+    "telefone": "65993271164",
+    "cidade": "Cuiabá",
+    "bairro": "Cinturão verde"
+  },
+  {
+    "nome": "Everton de souza Lourenço",
+    "telefone": "65981008560",
+    "cidade": "Cuiabá",
+    "bairro": "Don Aquino"
+  },
+  {
+    "nome": "Luciane Castorina mattos",
+    "telefone": "65993448972",
+    "cidade": "Várzea Grande",
+    "bairro": "Mapim"
+  },
+  {
+    "nome": "Orlando de souza",
+    "telefone": "65992448685",
+    "cidade": "Cuiabá",
+    "bairro": "Novo matogrosso"
+  }
+];
