b4e6ea7708e2ee2afa58dff8b25380930d9b8803
OSSJU <Junior.sereia@hotmail.com>
2026-09-07 17:41:54 -0400

    chore(admin): point to WhatsApp operations API

--- admin/config.js
+++ admin/config.js
@@ -2,6 +2,7 @@ window.DA_ADMIN_V3_CONFIG = Object.freeze({
   supabaseUrl: 'https://ssbesxgaijknwsjbsbcz.supabase.co',
   supabasePublishableKey: 'sb_publishable_tFXHtH0HCXZepVtwgKElIg_DxS76Gu8',
   edgeFunction: 'admin-ops-v1',
+  whatsappOpsEdgeFunction: 'admin-whatsapp-ops-v1',
   countAppUrl: '../contagem/',
-  build: '20260907-admin-oficial-01'
+  build: '20260907-admin-oficial-02'
 });
