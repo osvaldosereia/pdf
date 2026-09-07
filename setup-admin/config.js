45e1b1bb187a7c06d8f395788326cc27c051b9b6
OSSJU <Junior.sereia@hotmail.com>
2026-09-07 07:26:24 -0400

    Configura bootstrap do Admin no Supabase

---
+++ setup-admin/config.js
@@ -0,0 +1,5 @@
+window.DA_SETUP_ADMIN_CONFIG = Object.freeze({
+  supabaseUrl: 'https://ssbesxgaijknwsjbsbcz.supabase.co',
+  supabasePublishableKey: 'sb_publishable_tFXHtH0HCXZepVtwgKElIg_DxS76Gu8',
+  bootstrapFunction: 'bootstrap-owner-v1'
+});
