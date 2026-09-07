88b22ac3a5f55b8e747ca1916a5da77873669416
OSSJU <Junior.sereia@hotmail.com>
2026-09-07 00:56:13 -0400

    Adiciona configuração pública do novo Admin v3

---
+++ admin-v3/config.js
@@ -0,0 +1,7 @@
+window.DA_ADMIN_V3_CONFIG = Object.freeze({
+  supabaseUrl: 'https://ssbesxgaijknwsjbsbcz.supabase.co',
+  supabasePublishableKey: 'sb_publishable_tFXHtH0HCXZepVtwgKElIg_DxS76Gu8',
+  edgeFunction: 'admin-ops-v1',
+  countAppUrl: '../contagem-v2/',
+  build: '20260907-admin-v3-01'
+});
