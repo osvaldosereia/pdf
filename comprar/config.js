32c1653b8c205bb203e52df485e50fbbf9672358
OSSJU <Junior.sereia@hotmail.com>
2026-09-07 11:37:40 -0400

    Configura API da Sala de Compra

---
+++ comprar/config.js
@@ -0,0 +1,5 @@
+window.DA_SHOPPING_ROOM_CONFIG=Object.freeze({
+  api:'https://ssbesxgaijknwsjbsbcz.supabase.co/functions/v1/shopping-room-v1',
+  whatsappFallback:'https://wa.me/556584491018',
+  build:'20260907-shopping-room-01'
+});
