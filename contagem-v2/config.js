fe1691018fd1a5901851a56e1642dba090793714
OSSJU <Junior.sereia@hotmail.com>
2026-09-07 00:39:54 -0400

    Adiciona configuração pública da contagem v2

---
+++ contagem-v2/config.js
@@ -0,0 +1,9 @@
+window.DA_COUNT_CONFIG = Object.freeze({
+  supabaseUrl: 'https://ssbesxgaijknwsjbsbcz.supabase.co',
+  supabasePublishableKey: 'sb_publishable_tFXHtH0HCXZepVtwgKElIg_DxS76Gu8',
+  edgeFunction: 'inventory-count-v2',
+  firebaseUrl: 'https://cedar-chemist-310801-default-rtdb.firebaseio.com',
+  firebaseProductsNode: 'produtos',
+  fallbackCatalogUrl: '../site/produtos-admin.json',
+  build: '20260907-count-v2-01'
+});
