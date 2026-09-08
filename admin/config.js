05b9f3927461309f987138fe4a308c36d82f2727
OSSJU <Junior.sereia@hotmail.com>
2026-09-07 20:14:41 -0400

    config: preparar orquestrador admin atrás de feature flag

--- admin/config.js
+++ admin/config.js
@@ -3,6 +3,8 @@ window.DA_ADMIN_V3_CONFIG = Object.freeze({
   supabasePublishableKey: 'sb_publishable_tFXHtH0HCXZepVtwgKElIg_DxS76Gu8',
   edgeFunction: 'admin-ops-v1',
   whatsappOpsEdgeFunction: 'admin-whatsapp-ops-v1',
+  experienceOrchestratorEdgeFunction: 'admin-experience-orchestrator-v1',
+  experienceOrchestratorUiEnabled: false,
   countAppUrl: '../contagem/',
-  build: '20260907-admin-oficial-02'
+  build: '20260908-admin-orchestrator-foundation-01'
 });
