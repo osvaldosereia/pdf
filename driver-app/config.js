52d0f361efdd64787eac95e73b60fab3e0aad8ff
OSSJU <Junior.sereia@hotmail.com>
2026-09-08 07:16:39 -0400

    stage11: explicitar gate local de GPS do PWA

--- driver-app/config.js
+++ driver-app/config.js
@@ -1,8 +1,9 @@
 window.DA_DRIVER_CONFIG=Object.freeze({
   enabled:false,
+  gpsEnabled:false,
   supabaseUrl:'https://ssbesxgaijknwsjbsbcz.supabase.co',
   supabasePublishableKey:'sb_publishable_tFXHtH0HCXZepVtwgKElIg_DxS76Gu8',
   edgeFunction:'driver-logistics-v1',
   gpsIntervalSeconds:30,
-  build:'20260908-stage11-driver-pwa-01'
+  build:'20260908-stage11-driver-pwa-02'
 });
